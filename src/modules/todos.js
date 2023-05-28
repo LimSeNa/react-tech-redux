fb7336ecd5e732b70eca79a8707b293b6d022f08
Sena Lim <tpsk8322@naver.com>
2023-05-28 21:26:46 +0900

    immer 라이브러리 사용

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,4 +1,5 @@
 import {createAction, handleActions} from "redux-actions";
+import {produce} from "immer";
 
 // 액션 타입
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // input 값 변경
@@ -40,19 +41,24 @@ const initialState = {
 // 리듀서 함수
 const todos = handleActions(
     {
-        [CHANGE_INPUT]: (state, {payload: input}) => ({ ...state, input}),
-        [INSERT]: (state, {payload: todo}) => ({
-            ...state,
-            todos: state.todos.concat(todo),
+        [CHANGE_INPUT]: (state, {payload: input}) =>
+            produce(state, draft => {
+                draft.input = input;
+            }),
+        [INSERT]: (state, {payload: todo}) =>
+            produce(state, draft => {
+                draft.todos.push(todo);
+            }),
+        [TOGGLE]: (state, {payload: id}) =>
+            produce(state, draft => {
+            const todo = draft.todos.find(todo => todo.id === id);
+            todo.done = !todo.done;
         }),
-        [TOGGLE]: (state, {payload: id}) => ({
-            ...state,
-            todos: state.todos.map(todo => todo.id === id ? { ...state, done: !todo.done} : todo,),
-        }),
-        [REMOVE]: (state, {payload: id}) => ({
-            ...state,
-            todos: state.todos.filter(todo => todo.id !== id),
-        }),
+        [REMOVE]: (state, {payload: id}) =>
+            produce(state, draft => {
+                const index = draft.todos.findIndex(todo => todo.id === id);
+                draft.todos.splice(index, 1);
+            }),
     },
     initialState,
 );
