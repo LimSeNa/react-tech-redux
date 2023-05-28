0d01d64b8f0fadf471368c469ab38f2286d08ed6
Sena Lim <tpsk8322@naver.com>
2023-05-28 21:35:49 +0900

    useSelect Hook을 사용하여 상태 조회

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,67 +1,10 @@
 import Counter from "../components/Counter";
-import {connect} from "react-redux";
-import {decrease, increase} from "../modules/counter";
+import {useSelector} from "react-redux";
 
-const CounterContainer = ({ number, increase, decrease }) => {
-    return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-    );
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+
+    return <Counter number={number}/>;
 };
 
-/*
-const mapStateToProps = state => ({
-    number: state.counter.number,
-});
-
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        dispatch(increase());
-    },
-    decrease: () => {
-        dispatch(decrease());
-    },
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(CounterContainer);
-*/
-
-/*
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    dispatch => ({
-        increase: () => dispatch(increase()),
-        decrease: () => dispatch(decrease()),
-    }),
-)(CounterContainer);
-*/
-
-/*
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    dispatch =>
-        bindActionCreators(
-            {
-                increase,
-                decrease,
-            },
-            dispatch,
-        ),
-)(CounterContainer);
-*/
-
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    {
-        increase,
-        decrease,
-    },
-)(CounterContainer);
+export default CounterContainer;
