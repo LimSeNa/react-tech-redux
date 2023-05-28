937e6d6a9990bd4c4e4b284bba915d34947e888a
Sena Lim <tpsk8322@naver.com>
2023-05-28 16:33:18 +0900

    스토어에 접근하는 CounterContainer 컴포넌트

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,67 @@
+import Counter from "../components/Counter";
+import {connect} from "react-redux";
+import {decrease, increase} from "../modules/counter";
+
+const CounterContainer = ({ number, increase, decrease }) => {
+    return (
+        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    );
+};
+
+/*
+const mapStateToProps = state => ({
+    number: state.counter.number,
+});
+
+const mapDispatchToProps = dispatch => ({
+    increase: () => {
+        dispatch(increase());
+    },
+    decrease: () => {
+        dispatch(decrease());
+    },
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(CounterContainer);
+*/
+
+/*
+export default connect(
+    state => ({
+        number: state.counter.number,
+    }),
+    dispatch => ({
+        increase: () => dispatch(increase()),
+        decrease: () => dispatch(decrease()),
+    }),
+)(CounterContainer);
+*/
+
+/*
+export default connect(
+    state => ({
+        number: state.counter.number,
+    }),
+    dispatch =>
+        bindActionCreators(
+            {
+                increase,
+                decrease,
+            },
+            dispatch,
+        ),
+)(CounterContainer);
+*/
+
+export default connect(
+    state => ({
+        number: state.counter.number,
+    }),
+    {
+        increase,
+        decrease,
+    },
+)(CounterContainer);
