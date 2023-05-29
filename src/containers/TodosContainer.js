ef91b468ec9c57a1390ebbda5509d28f73d8f847
Sena Lim <tpsk8322@naver.com>
2023-05-29 11:11:52 +0900

    useActions Hook 사용

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,7 +1,7 @@
 import Todos from "../components/Todos";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {changeInput, insert, remove, toggle} from "../modules/todos";
-import {useCallback} from "react";
+import useActions from "../lib/useActions";
 
 const TodosContainer = () => {
     const {input, todos} = useSelector(({todos}) => ({
@@ -9,11 +9,10 @@ const TodosContainer = () => {
         todos: todos.todos
     }));
 
-    const dispatch = useDispatch();
-    const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
-    const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
-    const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
-    const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+    const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+        [changeInput, insert, toggle, remove],
+        []
+    );
 
     return (
         <Todos
